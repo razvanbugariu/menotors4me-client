c2716f8f91a175581d5503c3fa715154f78cc225
Gheorghe Popescu <gheorghe.popescu@unifiedpost.com>
2017-02-09 20:17:03 +0200

    moved create organization to header and changed select skills

--- app/components/authentication/login.controller.js
+++ app/components/authentication/login.controller.js
@@ -1,38 +1,32 @@
 'use strict';
 
 angular
-	.module('mentors4me')
-	.controller('loginController', loginController);
+  .module('mentors4me')
+  .controller('loginController', loginController);
 
 function loginController($scope, $location, $rootScope, AUTH_EVENTS, loginService, Constants) {
 
-	$scope.errors = [];
-
+  $scope.errors = [];
   $scope.login = login;
-	$scope.goToRegister = goToRegister;
 
-	function goToRegister (){
-		$location.path("/register");
-	}
-
-	function login(){
+  function login() {
     var user = {
-      email : $scope.user.email,
-      password : $scope.user.password
+      email: $scope.user.email,
+      password: $scope.user.password
     };
     loginService.login(user);
   }
 
-	$rootScope.$on(AUTH_EVENTS.loginSuccess, function(event, args) {
-		$location.path(args);
-	});
+  $rootScope.$on(AUTH_EVENTS.loginSuccess, function(event, args) {
+    $location.path(args);
+  });
 
-	$rootScope.$on(AUTH_EVENTS.loginFailed, function(event, args) {
-		$scope.errors = args;
-	});
+  $rootScope.$on(AUTH_EVENTS.loginFailed, function(event, args) {
+    $scope.errors = args;
+  });
 
-	$rootScope.$on(AUTH_EVENTS.logoutFailed, function(event, args) {
-		$scope.errors = args;
-	});
+  $rootScope.$on(AUTH_EVENTS.logoutFailed, function(event, args) {
+    $scope.errors = args;
+  });
 
 }
