d816113f921f8b395c40028a99c82f2e4912cddd
Razvan <razvanbugariu@yahoo.com>
2017-02-13 23:04:45 +0200

    added tabs and some field in propose mentor form

--- app/components/proposal/proposal.controller.js
+++ app/components/proposal/proposal.controller.js
@@ -4,11 +4,25 @@ angular
   .module('mentors4me')
   .controller('proposalController', mentorProposalController);
 
-function mentorProposalController($scope, $location, proposalService, Constants, growl) {
+function mentorProposalController($scope, $location, proposalService, Constants, growl, $translate) {
 
-  $scope.errors = [];
 
   $scope.proposeMentor = proposeMentor;
+  $scope.saveUserAndGoNext = saveUserAndGoNext;
+
+  $scope.activeStep = 0;
+  $scope.errors = [];
+  $scope.tabs = [
+   {
+     title: $translate.instant('personal_details'),
+     templateUrl: 'app/components/proposal/personal.tab.html',
+     disable: true
+   },
+   {
+     title: $translate.instant('mentor_details'),
+     templateUrl: 'app/components/proposal/mentor.tab.html',
+     disable: true
+   }];
 
   function proposeMentor() {
     var proposal = {
@@ -26,4 +40,10 @@ function mentorProposalController($scope, $location, proposalService, Constants,
   function handleErrors(responseError) {
     $scope.errors = responseError.data.errors;
   }
+
+  function saveUserAndGoNext(user){
+    $scope.user = user;
+    $scope.activeStep = 1;
+  }
+
 }
